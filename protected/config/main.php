da179e6bb02647f884e82dede55d58d42ad124ad
Abhilash Tiwari <send.to.abhilash@gmail.com>
2020-09-15 04:35:01 -0400

    * Updating app name to bizmaker.notetub.com in protected/config/main.php, to separate notes of knowall, from others.

--- protected/config/main.php
+++ protected/config/main.php
@@ -9,7 +9,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Ava.NoteTub.com',
+	'name'=>'bizmaker.NoteTub.com',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
